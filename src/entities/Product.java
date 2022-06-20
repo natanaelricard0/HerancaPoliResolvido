06277c66f239fc322009124fc3bb6837c96e7404
Natanael Ricardo <nathanshurle@gmail.com>
2022-06-20 11:23:26 -0300

    classe de produtos importados adicionada

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -24,12 +24,12 @@ public class Product {
 	public void setPrice(Double price) {
 		this.price = price;
 	}
-
+	
 	public String priceTag() {
 		return name
 				+ " $ "
 				+ String.format("%.2f", price);
 	}
 	
-		
+	
 }
